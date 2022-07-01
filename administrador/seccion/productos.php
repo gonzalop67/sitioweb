901d6cc1e7c691eeb7a124b997b34c1873f098a6
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2022-07-01 06:27:38 -0500

    Enviando información con método post

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -16,13 +16,13 @@
                     <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
                 </div>
                 <div class="form-group">
-                    <label for="txtNombre">Imagen:</label>
-                    <input type="file" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
+                    <label for="txtImagen">Imagen:</label>
+                    <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="Nombre del libro">
                 </div>
                 <div class="btn-group" role="group" aria-label="">
-                    <button type="button" class="btn btn-success">Agregar</button>
-                    <button type="button" class="btn btn-warning">Modificar</button>
-                    <button type="button" class="btn btn-info">Cancelar</button>
+                    <button type="button" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                    <button type="button" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                    <button type="button" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
                 </div>
             </form>
         </div>
