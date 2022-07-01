71175eb6e928c500e10b74a9111c9e05dea8f450
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2022-07-01 06:06:19 -0500

    Interfaz formulario Libro

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,7 +1,32 @@
 <?php include("../template/cabecera.php"); ?>
 
 <div class="col-md-5">
-    Formulario de agregar libros
+    <div class="card">
+        <div class="card-header">
+            Datos de Libro
+        </div>
+        <div class="card-body">
+            <form method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="txtID">ID:</label>
+                    <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                </div>
+                <div class="form-group">
+                    <label for="txtNombre">Nombre:</label>
+                    <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
+                </div>
+                <div class="form-group">
+                    <label for="txtNombre">Imagen:</label>
+                    <input type="file" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
+                </div>
+                <div class="btn-group" role="group" aria-label="">
+                    <button type="button" class="btn btn-success">Agregar</button>
+                    <button type="button" class="btn btn-warning">Modificar</button>
+                    <button type="button" class="btn btn-info">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 <div class="col-md-7">
