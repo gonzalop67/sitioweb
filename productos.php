66d674bb816f0c8468effd31d467745c00ab9b7d
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2022-07-02 23:20:08 -0500

    Mostrando Libros en el sitio web

--- productos.php
+++ productos.php
@@ -1,43 +1,26 @@
-<?php include("template/cabecera.php"); ?>
+<?php
+include("template/cabecera.php");
+include("administrador/config/bd.php");
 
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Libro de PHP</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+$sentenciaSQL = $conexion->prepare("SELECT * FROM libros");
+$sentenciaSQL->execute();
+$listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+foreach ($listaLibros as $libro) {
+?>
+    <div class="col-md-3">
+        <div class="card">
+            <img class="card-img-top" src="./img/<?php echo $libro['imagen'] ?>" alt="">
+            <div class="card-body">
+                <h4 class="card-title"><?php echo $libro['nombre'] ?></h4>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Libro de PHP</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Libro de PHP</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-        </div>
-    </div>
-</div>
-
-<div class="col-md-3">
-    <div class="card">
-        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Libro de PHP</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
-        </div>
-    </div>
-</div>
+<?php
+}
+?>
 
 <?php include("template/pie.php"); ?>
